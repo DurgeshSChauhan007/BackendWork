597e04a2fae95ad93e09102b027e6530dec33268
DurgeshSChauhan007 <durgeshsinghchauhan97.4@gmail.com>
2025-04-03 09:35:30 +0530

    commit second part

--- httpModule/nodeRegisteration/app.js
+++ httpModule/nodeRegisteration/app.js
@@ -1,78 +1,77 @@
-const http = require('http')
-const fs = require('fs').promises;
+const http=require('http');
+const fs=require('fs').promises;
+const PORT=3002;
+const server=http.createServer((req,res)=>{
 
-const server = http.createServer((req, res) => {
-    console.log("Server starts");
-    console.log(req.url, req.method);
-
-    if (req.url === '/register' && req.method == 'POST') {
-
-        let body = '';
-        let arr = [];
-        req.on('data', chunk => {
-            body += chunk
-        })
-
-        req.on('end', async () => {
-            const {name, email, password } = JSON.parse(body);
-            console.log(name);
-            console.log(email);
-            console.log(password);
-
-            const fdata = await fs.readFile('student.json', {encoding:'utf-8'});
-                    arr = JSON.parse(fdata);
-                    console.log(arr);
-                    const result = arr.find(ele => ele.email == email);
-                    if (result) {
-                        res.setHeader('Content-Type', 'application/json');
-                        return res.end(JSON.stringify({"message" : 'This is already registered with us, Try other one.'}))
-                    }
-                    else {
-                        arr.push({name, email, password});
-
-                        await fs.writeFile('student.json', JSON.stringify(arr, null, 2));
-
-                        // console.log(body);
-
-                        return res.end(JSON.stringify({"message": "/register api hit successfully!!"}));
-                    }
-
-        })
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-method','GET,POST,DELETE,PUT,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+//preflight
+    if(req.method=="OPTIONS"){
+        res.statusCode=200;
+        return res.end();
     }
 
-    if (req.url === '/login' && req.method == 'POST') {
+   
 
-        let body = '';
-        let arr = [];
-        req.on('data', chunk => {
-            body += chunk
-        })
+if(req.url=="/register" && req.method=="POST"){
+    let body='';
+    let arr=[];
+    req.on('data',chunk=>{
+        body+=chunk;
+    })
 
-        req.on('end', async() => {
-            const {email, password} = JSON.parse(body);
-            const fdata = await fs.readFile('student.json', {encoding: 'utf-8'});
-            arr = JSON.parse(fdata);
-            console.log(arr);
+    req.on('end',async ()=>{
+        const {name,email,password}=JSON.parse(body);
+        console.log(name+email+password);
+        const fdata=await fs.readFile('student.json',{encoding:'utf-8'});
+                arr=JSON.parse(fdata);
+                console.log(arr);
+                  const result=arr.find(ele=>ele.email==email);
+                  if(result){
+                    res.setHeader('Content-Type','application/json');
+                    return res.end(JSON.stringify({"message":"This email is already registerd with us, pls try different email"}))
+                  }
+               else{
+                arr.push({name,email,password});
 
-            const result = arr.find(ele => ele.email == email && ele.password == password);
-            if (result) {
-                res.setHeader('Content-Type', 'application/json');
-                return res.end(JSON.stringify({"message" : "Login Successfully"}))
-            }
-            else {
-                res.setHeader('Content-Type', 'application/json');
-                return res.end(JSON.stringify({"message" : "Not registered"}));
-            }
-        })
+                await fs.writeFile('student.json',JSON.stringify(arr,null,2));
+                res.end(JSON.stringify({"message":"Registration successfully!!"}));
+               }
+               
+        
+        //console.log(body);
+    })
+    //res.end(JSON.stringify({"message":"/register api hit successfully!!"}));
+}
+
+if(req.url=='/login' && req.method=="POST"){
+    let body='';
+    let arr=[];
+    req.on('data',chunk=>{
+        body+=chunk;
+    })
+
+    req.on('end',async()=>{
+        const {email,password}=JSON.parse(body);
+        const fdata=await fs.readFile('student.json',{encoding:'utf-8'});
+                arr=JSON.parse(fdata);
+                console.log(arr);
+                const result=arr.find(ele=>ele.email==email && ele.password==password);
+                if(result){
+                       res.setHeader('Content-Type','application/json');
+                       res.end(JSON.stringify({"message":"Login Success!!"}));
+                }else{
+                    res.setHeader('Content-Type','application/json');
+                    res.end(JSON.stringify({"message":"email or password is incorrect!!"}));
+                }
+
+    })
+}
+//res.end("Welocme to Node Server");
 
 
-    }
-    // res.end('Welcome to the node');
 })
-
-const PORT = 4001;
-
-server.listen(PORT, () => {
-    console.log(`Server running at address ${PORT}`);
-});
-
+server.listen(PORT,()=>{
+    console.log("Server is running on:"+PORT)
+})
